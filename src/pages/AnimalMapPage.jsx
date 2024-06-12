9f9d1a6d8ebbbb642d4d322db1817a8e09748f19
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-06-12 14:33:20 +0300

    Minor Changes

--- src/pages/AnimalMapPage.jsx
+++ src/pages/AnimalMapPage.jsx
@@ -1,6 +1,6 @@
 // pages/AnimalMapPage.js
 import { Box } from '@chakra-ui/react';
-import AnimalMap from '../components/MapComponent3';
+import AnimalMap from '../components/MapComponent2';
 
 const AnimalMapPage = () => (
   <Box>
